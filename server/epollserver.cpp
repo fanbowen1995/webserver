2d8253820600957810d4b7f9a492e09185275bd2
fanbowen <379186207@qq.com>
2021-02-27 03:22:30 +0800

    -

--- server/epollserver.cpp
+++ server/epollserver.cpp
@@ -88,7 +88,7 @@ void CEpollServer::dealClientAccept() {
     {
         printf("errno : accept error.\n");
     }
-    add_event(epollfd, connfd, EPOLLIN | EPOLLOUT | EPOLLET);
+    add_event(epollfd, connfd, EPOLLIN | EPOLLET);
     connections[connfd].fd = connfd;
     printf("dealClientAccept exit.\n");
 }
